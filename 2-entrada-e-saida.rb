59c9321932c0000fe90ac12f130225e43965c856
marlon <marloncostaesousa@gmail.com>
2024-03-20 12:15:18 -0300

    Adicionando conteúdo sobre primeiro código, tipos primitivos e variáveis, entrada e saída padrão

---
+++ 2-entrada-e-saida.rb
@@ -0,0 +1,16 @@
+puts "Olá"
+puts "Digite seu nome:"
+nome = gets.chomp
+puts "Seu nome é " + nome.inspect
+
+puts "-------------------------------------"
+
+puts "Digite seu sobrenome:"
+sobrenome = gets
+puts "Seu sobrenome é " + sobrenome.inspect
+
+puts "-------------------------------------"
+
+puts "Digite sua idade:"
+idade = gets.chomp
+puts "Sua idade é " + idade
